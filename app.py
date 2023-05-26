8f540b3a2e8cc6f7a44f029f6472c7a968d79f68
Antonio Carlos Souza de Abrantes <47427886+antonioabrantes@users.noreply.github.com>
2023-05-25 22:20:17 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# https://www.youtube.com/watch?v=K2ejI4z8Mbg&t=701s
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+	return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+	return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+	return render_template("usuarios.html",nome_usuario=nome_usuario)
+
+if __name__ == "__app__":
+	#app.run(debug=True)
+	app.run(host='0.0.0.0', port=8080)
